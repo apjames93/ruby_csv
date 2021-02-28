a21919a1ead73e9fca44d20889653e4b0f669166
alex james <apjames93@gmail.com>
2021-02-27 20:50:52 -0700

    format numbers for E.164 and test

--- lib/ruby_csv.rb
+++ lib/ruby_csv.rb
@@ -3,18 +3,39 @@ require 'csv'
 module RubyCsv
   def self.parse
     csv_array = CSV.parse(File.read("data/input.csv"), headers: true)
-    csv_array.map { |data| clean(data) }
-    puts csv_array
+    format_data = csv_array.map { |data| clean(data) }
+    puts format_data
   end
 
-    # loops over key values and formats key values
-    def self.clean(data)
-      hash = {}
-      data.each do |key, v|
-        value =  v.nil? ? '' : v.strip
-        hash[key] = value
+  # private
+
+  # loops over key values and formats key values
+  def self.clean(data)
+    hash = {}
+    data.each do |key, v|
+    # removes all white space
+    value = v.nil? ? '' : v.gsub(/ /, '')
+
+      if key == 'phone_number'
+          hash[key] = phone_number_format(value)
+      else
+          hash[key] = value
       end
-      hash
     end
+    hash
+  end
+
+  # formats number to E.164
+  def self.phone_number_format(number)
+    # removes all values that are not 0-9 and then take the last 10 characters from the string
+    formatted_number = (number || '').tr('^0-9', '').split(//).last(10).join('').to_s
+    formatted_number = '+1'.concat(formatted_number)
+    formatted_number.length != 12 ? 'Invalid number' : formatted_number
+  end
+
+  private_class_method(
+    :phone_number_format,
+    :clean
+  )
 
 end
--- spec/ruby_csv_spec.rb
+++ spec/ruby_csv_spec.rb
@@ -3,7 +3,18 @@ RSpec.describe RubyCsv do
     expect(RubyCsv::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "phone_number_format returns number to be E.164 format or return invalid number" do
+    number = RubyCsv.send(:phone_number_format, '(303) 887 3456')
+    expect(number).to eq('+13038873456')
+
+    number = RubyCsv.send(:phone_number_format, '303-333-9987')
+    expect(number).to eq('+13033339987')
+
+    number = RubyCsv.send(:phone_number_format, '13039873345')
+    expect(number).to eq('+13039873345')
+
+    number = RubyCsv.send(:phone_number_format, nil)
+    expect(number).to eq('Invalid number')
   end
+
 end
