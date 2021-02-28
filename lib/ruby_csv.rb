01e134dfbc4f94d8ce2469dd94f224b5c9d99881
alex james <apjames93@gmail.com>
2021-02-27 20:24:11 -0700

    read input.csv and remove whitespace from hash values

--- lib/ruby_csv.rb
+++ lib/ruby_csv.rb
@@ -1,6 +1,20 @@
-require "ruby_csv/version"
+require 'csv'
 
 module RubyCsv
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.parse
+    csv_array = CSV.parse(File.read("data/input.csv"), headers: true)
+    csv_array.map { |data| clean(data) }
+    puts csv_array
+  end
+
+    # loops over key values and formats key values
+    def self.clean(data)
+      hash = {}
+      data.each do |key, v|
+        value =  v.nil? ? '' : v.strip
+        hash[key] = value
+      end
+      hash
+    end
+
 end
